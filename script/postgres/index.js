bd3adb6b9796e6c6cb6ae3a56048900e40192c5a
Natalie <swcwsncyt@gmail.com>
2019-09-24 13:16:35 -0700

    AWS config

--- script/postgres/index.js
+++ script/postgres/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'ec2-52-53-154-245.us-west-1.compute.amazonaws.com',
+  host: 'ec2-13-57-17-213.us-west-1.compute.amazonaws.com',
   database: 'reviewsDB',
   user: 'power_user',
   password: 'superpassword',
