41594ad595ec15bbf0731685ff6a621aa9d4ee73
Natalie <swcwsncyt@gmail.com>
2019-09-26 15:13:08 -0700

    loader config

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ require('newrelic');
 
 const PORT = 3003;
 
-app.use(express.static(path.join(__dirname,  '../public/loader')));
+// app.use(express.static(path.join(__dirname,  '../public/loader')));
 app.use('/:id/', express.static(path.join(__dirname,  '../public')));
 
 app.use(bodyParser.json());
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get('/loaderio-0adb952a970d2b41d6af1319e9730311/', (req, res) => {
-//   res.send('loaderio-0adb952a970d2b41d6af1319e9730311');
-// })
+app.get('/loaderio-0adb952a970d2b41d6af1319e9730311/', (req, res) => {
+  res.send('loaderio-0adb952a970d2b41d6af1319e9730311');
+})
 
 app.get('/api/:restID/reviews', (req, res) => {
   db.getReviews(req.params.restID, (reviews) => {
